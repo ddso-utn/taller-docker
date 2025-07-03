f880ed3c741aeaacd41b49f43d1de394e2b5db92
Franco Leonardo Bulgarelli <franco@mumuki.org>
2025-07-03 00:00:53 -0300

    Versión con gestión de recursos

--- hola-mundo-http/index.js
+++ hola-mundo-http/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 8080;
+const port = 8080 ;
 
-app.get('/hola', (req, res) => {
+const archivoAcercaDe = "acercade.txt";
+
+app.get('/hola', async (req, res) => {
   const quien = req.query.quien ?? 'Mundo';
+
   console.log(`Respondiendo al saludo de ${req.query.quien}`)
-  res.send(`¡Hola ${quien}!`);
+
+  // Cuidado: el uso de existSync y readFileSync
+  // es sólo a fines didácticos y no se recomienda en aplicaciones reales
+  const acercaDe = fs.existsSync(archivoAcercaDe) ? fs.readFileSync(archivoAcercaDe) : "";
+  const saludo = process.env.SALUDO ?? "Hola"
+
+  res.send(`¡${saludo} ${quien}! ${acercaDe}`);
+
 });
 
 app.listen(port, () => {
